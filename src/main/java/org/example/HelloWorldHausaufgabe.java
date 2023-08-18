d4652c50742703842b3e129b316c3327eb29aed7
Martin Ortner <m.ortner76@googlemail.com>
2023-08-18 19:12:42 +0200

    Add new feature

--- src/main/java/org/example/HelloWorldHausaufgabe.java
+++ src/main/java/org/example/HelloWorldHausaufgabe.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class HelloWorldHausaufgabe {
 
     public static void main(String[] args) {
+        System.out.println("New feature");
     }
 }
