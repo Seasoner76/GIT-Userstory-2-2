c527911b9b107b703cfbcb45988bf412898adfdc
Martin Ortner <m.ortner76@googlemail.com>
2023-08-18 18:51:16 +0200

    Helloworld

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+      // <Datentyp> <Variablenname> = <Wert>
+        boolean e = true; // logische Werte
+        char c = 'r'; // Buchstaben
+        short s = 4200;
+        int i = 420000000;
+        long l =4200000000000000L;
+        float f = 42.94F;
+        double d = 42.9403940034;
+        byte b1 = 127;
+
+        final boolean b = true;
+        b = false;
+
+    }
+
+}
+
+
