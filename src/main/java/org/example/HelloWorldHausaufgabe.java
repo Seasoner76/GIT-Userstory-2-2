bfe7fb98baa6390ee6abc514cee13ddbae9ca9a4
Martin Ortner <m.ortner76@googlemail.com>
2023-08-21 09:16:46 +0200

    completed code

--- src/main/java/org/example/HelloWorldHausaufgabe.java
+++ src/main/java/org/example/HelloWorldHausaufgabe.java
@@ -3,6 +3,23 @@ package org.example;
 public class HelloWorldHausaufgabe {
 
     public static void main(String[] args) {
-        System.out.println("New feature");
+
+        final double d = 35.9234720837293723;
+
+        final boolean b = false;
+
+        final short s = 7900;
+
+        final char c ='z';
+
+        final float f = 49.78F;
+
+        final int i = 830000000;
+
+        final byte g = 112;
+
+        final long l = 890000000000000000L;
+
+        System.out.println("Hello world Homework" +d+b+c+s+g+f+i+l);
     }
 }
