41e59ef4b8bdc45441829ce11fc22f73475449ff
Martin Ortner <m.ortner76@googlemail.com>
2023-09-06 09:30:58 +0200

    HelloWorldHausaufgabelatestcommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-      // <Datentyp> <Variablenname> = <Wert>
-        boolean e = true; // logische Werte
-        char c = 'r'; // Buchstaben
-        short s = 4200;
-        int i = 420000000;
-        long l =4200000000000000L;
-        float f = 42.94F;
-        double d = 42.9403940034;
-        byte b1 = 127;
+        // <Datentyp> <Variablenname> = <Wert>
+        final double d = 35.9234720837293723;
 
-        final boolean b = true;
-        b = false;
+        final boolean b = false;
+
+        final short s = 7900;
+
+        final char c = 'z';
+
+        final float f = 49.78F;
+
+        final int i = 830000000;
+
+        final byte g = 112;
+
+        final long l = 890000000000000000L;
+
+        System.out.println("Hello world Homework" + d + b + c + s + g + f + i + l);
 
     }
 
